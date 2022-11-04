96c656b6e8f813c0385f57a1d828912726f7cb50
mango0422 <tom990422@gmail.com>
2022-11-04 18:36:51 +0900

    2022-11-04

---
+++ main.java
@@ -0,0 +1,13 @@
+public class main(){
+    public boolean JoinIn(String ID, String pwd, String, name, String quize1, String quize2);
+    public boolean logIn(String ID, String pwd);
+    public boolean changePwd(int id, String newPwd);
+    public void follow(int id, int following_id);
+    public int showFollower(int id);
+    public int showFollowing(int id);
+    public boolean posting(int id, String content);
+    public boolean comment(int p_id, int id, String content);
+    public static void main(String[] args){
+        
+    }
+}
